b9c420f5e95001901b2b92f33a90c3be48e0d8b3
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-11-21 11:50:24 +0100

    add view page tabs

--- dashview/views.py
+++ dashview/views.py
@@ -5,3 +5,7 @@ from teles.models import Agent
 from django.http import JsonResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+
+def dashview(request):
+    username = request.user.username
+    return render(request, 'dashview.html',{'username':username})

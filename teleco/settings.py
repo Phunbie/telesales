58794590fa8c5f8104de59ae00af7238a384a453
Phunbie <adeneyeoluwafunbi@gmail.com>
2023-11-23 07:30:14 +0100

    add feedback section

--- teleco/settings.py
+++ teleco/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-4^#iiz%-0&_1geh%fgvj=pw(4skp8l%cs_aunssw=8e_2edq^@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '7158-102-88-33-88.ngrok-free.app']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '40f7-102-88-33-23.ngrok-free.app']
 
 
 
